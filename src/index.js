7aeb0279735b2137dd83486ae62063ad9ba68054
Diego Collao <dcollao@icloud.com>
2017-10-21 01:39:42 -0300

    passing data through props

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//Square Component
+class Square extends React.Component {
+  render() {
+    return (
+      //Create a button with the value of this.renderSquare(n)
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+
+//Board
+class Board extends React.Component {
+
+  //Here i'm rendering the Square on the board.
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Siguiente jugador: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+//Game is the main class
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+//Render all the content in the id root !
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
